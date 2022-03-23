e4691afa78da244e670f9ab65d3b335752138116
tomonari <zcsxcacg@i.softbank.jp>
2022-03-23 14:05:23 +0900

    DBへの接続を変数に入れて使いまわせるように変更、@memosも戻り値を代入する形に変更

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 1. PostgreSQLに接続し、`CREATE DATABASE memo;`でデータ保存用のDBを作ります。
 1. 作ったデータベース(MEMO)に移動し、以下のDDL文を実行しデータ保存用のテーブルを作ります。
 `CREATE TABLE memos (id SERIAL NOT NULL, title VARCHAR NOT NULL, content VARCHAR, PRIMARY KEY (id));`
-1. `bundle install`を実行し、sinatra、sinatra-contrib,pgのgemをインストールします。
+1. `bundle install`を実行し、sinatra、sinatra-contrib,pg,webrickのgemをインストールします。
 1. ruby memos.rbを実行し、Sinatraを起動します。
 1. ブラウザで<http://localhost:4567/memos>にアクセスします。
 1. `追加`でメモを新しく作り投稿します。
--- memos.rb
+++ memos.rb
@@ -5,39 +5,38 @@ require 'sinatra/reloader'
 require 'pg'
 require 'cgi'
 
+def connect_database
+  PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
+end
+
 def generate_memos(id, title, content)
-  connect = PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
-  @memos = { 'id': id, 'title': title, 'content': content }
+  { id: id, title: title, content: content }
 end
 
-def load_all_memos
-  connect = PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
-  @memos = connect.exec('SELECT * FROM memos ORDER BY id ASC')
+def load_all_memos(connect)
+  connect.exec('SELECT * FROM memos ORDER BY id ASC')
 end
 
-def load_detail_memo(id)
-  connect = PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
-  @memos = connect.exec('SELECT * FROM memos WHERE id = $1', id)
+def load_detail_memo(connect, id)
+  connect.exec('SELECT * FROM memos WHERE id = $1', id)
 end
 
-def save_new_memo
-  connect = PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
+def save_new_memo(connect)
   connect.exec('INSERT INTO memos(title, content) VALUES ($1, $2)', [@memos[:title], @memos[:content]])
 end
 
-def update_memo
-  connect = PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
+def update_memo(connect)
   connect.exec('UPDATE memos SET title = $1, content = $2 WHERE id = $3',
                [@memos[:title], @memos[:content], @memos[:id]])
 end
 
-def delete_memo(id)
-  connect = PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
+def delete_memo(connect, id)
   connect.exec('DELETE FROM memos WHERE id = $1', id)
 end
 
 get '/memos' do
-  load_all_memos
+  connection = connect_database
+  @memos = load_all_memos(connection)
   @title = 'index'
   erb :index
 end
@@ -48,30 +47,35 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  load_detail_memo([params[:id]])
+  connection = connect_database
+  @memos = load_detail_memo(connection, [params[:id]])
   @title = 'detail'
   erb :detail
 end
 
 post '/memos' do
-  generate_memos([params[:id]], params[:title], params[:content])
-  save_new_memo
+  connection = connect_database
+  @memos = generate_memos([params[:id]], params[:title], params[:content])
+  save_new_memo(connection)
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
-  load_detail_memo([params[:id]])
+  connection = connect_database
+  @memos = load_detail_memo(connection, [params[:id]])
   @title = 'edit'
   erb :edit
 end
 
 patch '/memos/:id' do
-  generate_memos(params[:id], params[:title], params[:content])
-  update_memo
+  connection = connect_database
+  @memos = generate_memos(params[:id], params[:title], params[:content])
+  update_memo(connection)
   redirect '/memos'
 end
 
 delete '/memos/:id' do
-  delete_memo([params[:id]])
+  connection = connect_database
+  delete_memo(connection, [params[:id]])
   redirect '/memos'
 end
