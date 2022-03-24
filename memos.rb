50aac2b1a90ae241bbd62374f9945c621ba61179
tomonari <zcsxcacg@i.softbank.jp>
2022-03-24 14:35:25 +0900

    メソッドをまとめてクラス化した

--- memos.rb
+++ memos.rb
@@ -5,38 +5,39 @@ require 'sinatra/reloader'
 require 'pg'
 require 'cgi'
 
-def connect_database
-  PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
+class Memo 
+  @connect = PG::Connection.new(host: 'localhost', user: 'postgres', dbname: 'memo')
+
+  def create(id, title, content)
+    { id: id, title: title, content: content }
+  end  
+
+  def all
+    @connect.exec('SELECT * FROM memos ORDER BY id ASC')
+  end  
+
+  def detail(id)
+    @connect.exec('SELECT * FROM memos WHERE id = $1', id)
+  end  
+
+  def save(memo)
+    @connect.exec('INSERT INTO memos(title, content) VALUES ($1, $2)', [memo[:title], memo[:content]])
+  end  
+
+  def update(memo)
+    @connect.exec('UPDATE memos SET title = $1, content = $2 WHERE id = $3',
+                 [memo[:title], memo[:content], memo[:id]])
+  end  
+
+  def destroy(id)
+    @connect.exec('DELETE FROM memos WHERE id = $1', id)
+  end
 end
 
-def generate_memo(id, title, content)
-  { id: id, title: title, content: content }
-end
-
-def load_all_memos(connect)
-  connect.exec('SELECT * FROM memos ORDER BY id ASC')
-end
-
-def load_detail_memo(connect, id)
-  connect.exec('SELECT * FROM memos WHERE id = $1', id)
-end
-
-def save_new_memo(connect, memo)
-  connect.exec('INSERT INTO memos(title, content) VALUES ($1, $2)', [memo[:title], memo[:content]])
-end
-
-def update_memo(connect, memo)
-  connect.exec('UPDATE memos SET title = $1, content = $2 WHERE id = $3',
-               [memo[:title], memo[:content], memo[:id]])
-end
-
-def delete_memo(connect, id)
-  connect.exec('DELETE FROM memos WHERE id = $1', id)
-end
+memos = Memo.new
 
 get '/memos' do
-  connection = connect_database
-  @memos = load_all_memos(connection)
+  @memos = memos.all
   @title = 'index'
   erb :index
 end
@@ -47,35 +48,30 @@ get '/memos/new' do
 end
 
 get '/memos/:id' do
-  connection = connect_database
-  @memo = load_detail_memo(connection, [params[:id]])
+  @memo = memos.detail([params[:id]])
   @title = 'detail'
   erb :detail
 end
 
 post '/memos' do
-  connection = connect_database
-  memo = generate_memo([params[:id]], params[:title], params[:content])
-  save_new_memo(connection, memo)
+  memo = memos.create(params[:id], params[:title], params[:content])
+  memos.save(memo)
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
-  connection = connect_database
-  @memo = load_detail_memo(connection, [params[:id]])
+  @memo = memos.detail([params[:id]])
   @title = 'edit'
   erb :edit
 end
 
 patch '/memos/:id' do
-  connection = connect_database
-  memo = generate_memo(params[:id], params[:title], params[:content])
-  update_memo(connection, memo)
+  memo = memos.create(params[:id], params[:title], params[:content])
+  memos.update(memo)
   redirect '/memos'
 end
 
 delete '/memos/:id' do
-  connection = connect_database
-  delete_memo(connection, [params[:id]])
+  memos.destroy([params[:id]])
   redirect '/memos'
 end
